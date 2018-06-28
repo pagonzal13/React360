943939a81036b8623dae5659ec9a36adebfa51ed
pagonzal13 <paulagg176@gmail.com>
2018-06-29 01:55:29 +0200

    Bug background fixed

--- components/Background.js
+++ components/Background.js
@@ -12,10 +12,10 @@ export default class Background extends React.Component {
   }
   componentWillReceiveProps(nextProps) {
     //console.log("Está recibiendo nuevas props");
-    if ( nextProps.imgBack !== this.props.imgBack ) {
-      Environment.setBackgroundImage(asset(nextProps.imgBack), {format: nextProps.format});
-    } else if ( nextProps.urlBack !== this.props.urlBack ){
+    if ( nextProps.urlBack !== this.props.urlBack ){
       Environment.setBackgroundImage(nextProps.urlBack, {format: nextProps.format});
+    }else if ( nextProps.imgBack !== this.props.imgBack ) {
+      Environment.setBackgroundImage(asset(nextProps.imgBack), {format: nextProps.format});
     }
     
   }
